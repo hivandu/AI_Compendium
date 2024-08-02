f6fbeacda7ddfb8408ff4f491bd0043ab17d093f
Hivan Du <doo@hivan.me>
2024-08-02 16:30:53 +0800

    Math 01, Test the Latex

---
+++ Python/section_25.py
@@ -0,0 +1,14 @@
+'''
+Author: Hivan Du
+mail: doo@hivan.me
+LastEditors: Hivan Du
+LastEditTime: 2024-08-01 19:59:18
+'''
+# Import module
+import matplotlib.pyplot as plt
+# This code will display images when running in Jupyter
+# %matplotlib inline
+# Pass x and y to plot the graph
+plt.plot([1,0,9],[4,5,6])
+# Display the graph when executing the program
+plt.show()
